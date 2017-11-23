c8fbbac021ae310b24447645419944482cf3093e
Adibas03 <adibas03@ymail.com>
2017-11-23 12:35:59 +0100

    implement webpack and lint

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const settings = {
+  entry: {
+    bundle: [
+      "react-hot-loader/patch",
+      "./src/frontend/index.js"
+    ]
+  },
+  output: {
+    filename: "[name].js",
+    publicPath: "/js/",
+    path: path.resolve("dist/js")
+  },
+  resolve: {
+    extensions: [".js", ".json", ".css"]
+  },
+  devtool: "eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ["es2015", { modules: false }],
+            "stage-2",
+            "react"
+          ],
+          plugins: [
+            "transform-node-env-inline"
+          ],
+          env: {
+            development: {
+              plugins: ["react-hot-loader/babel"]
+            }
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              sourceMap: true,
+              importLoaders: 1,
+              localIdentName: "[name]--[local]--[hash:base64:8]"
+            }
+          },
+          "postcss-loader" // has separate config, see postcss.config.js nearby
+        ]
+      },
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve("src/www"),
+    publicPath: "http://localhost:8080/js", // full URL is necessary for Hot Module Replacement if additional path will be added.
+    quiet: false,
+    hot: true,
+    historyApiFallback: true,
+    inline: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      debug: true
+    })
+  ],
+};
+
+module.exports = settings;
