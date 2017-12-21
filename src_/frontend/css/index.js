d5d791d323283f8ccfc50a77542ae5cec54e218a
Adibas03 <adibas03@ymail.com>
2017-12-21 04:26:44 +0100

    clean up

--- src_/frontend/css/index.js
+++
@@ -1,9 +0,0 @@
-import Body from './Body.css';
-import Maincard from './Maincard.css';
-import Textfield from './Textfield.css';
-import TopBar from './TopBar.css';
-import Insidebox from './Insidebox.css';
-
-
-export default Body;
-export {Maincard, TopBar, Insidebox, Textfield};
