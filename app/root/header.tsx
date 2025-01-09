b62aca433c024828007183f3ac1de8efba982a1b
adibas03 <adibas03@ymail.com>
2025-01-09 15:00:10 +0100

    show web3 versions

--- app/root/header.tsx
+++ app/root/header.tsx
@@ -1,6 +1,8 @@
 import { NavLink } from "react-router";
 import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
+import packageInfo from "package-lock.json";
+import Descriptions from "app/actions/components/description";
 
 export default function Header() {
   return (
@@ -9,6 +11,13 @@ export default function Header() {
         <div className="w-[350px] max-w-[100vw] p-4 text-center">
           <div className="mt-8">
             <NavLink to="/">Online Ethereum abi encoder and decoder</NavLink>
+            <Descriptions>
+              web-eth@{packageInfo.packages["node_modules/web3-eth"].version}
+            </Descriptions>
+            <Descriptions>
+              web-eth-abi@
+              {packageInfo.packages["node_modules/web3-eth-abi"].version}
+            </Descriptions>
           </div>
         </div>
       </header>
